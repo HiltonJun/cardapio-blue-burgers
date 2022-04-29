0eddc50ba67f3e25036619bdcb1dfec366cc488a
HiltonJun <hilton.honda@gmail.com>
2022-04-29 19:59:01 -0300

    Feat: Modal para detalhes por id com css

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,6 +2,7 @@ import "./Home.css";
 import PaletaLista from "../../components/PaletaLista/PaletaLista";
 import Navbar from "../../components/Navbar/Navbar"
 import Overlay from "../../components/Overlay/Overlay"
+import Modal from "../../components/Modal/Modal"
 
 function Home() {
   return (
@@ -9,6 +10,8 @@ function Home() {
       <Navbar />
       <div className="Home__container">
         <PaletaLista />
+        <Overlay />
+        <Modal />
       </div>
     </div>
   );
